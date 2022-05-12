b8bd9779bca44753eac42a5edf1e8687cf815c2d
kkh97 <kgh970809@naver.com>
2022-05-12 23:09:43 +0900

    중력 개선, 네비게이션 배이킹
    주변 높이 평균으로 계산, 네비게이션 배이킹 새로함

---
+++ Assets/Scripts/Controllers/MonsterGravity.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterGravity : MonoBehaviour
+{
+    private int layerMask;
+
+    private Vector3 height;
+    private float radius;
+    private Vector3 width;
+
+    private RaycastHit[] hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerMask = 1 << LayerMask.NameToLayer("Ground");
+
+        height = new Vector3(0, 0.55f, 0);
+
+        hit = new RaycastHit[5];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position + height, Vector3.down, out hit[0], float.MaxValue, layerMask))
+        {
+            if (name == "Knight")
+            {
+                radius = GetComponent<CapsuleCollider>().radius;
+                Physics.Raycast(transform.position + height + new Vector3(radius, 0, 0), Vector3.down, out hit[1], float.MaxValue, layerMask);
+                Physics.Raycast(transform.position + height + new Vector3(-radius, 0, 0), Vector3.down, out hit[2], float.MaxValue, layerMask);
+                Physics.Raycast(transform.position + height + new Vector3(0, 0, radius), Vector3.down, out hit[3], float.MaxValue, layerMask);
+                Physics.Raycast(transform.position + height + new Vector3(0, 0, -radius), Vector3.down, out hit[4], float.MaxValue, layerMask);
+
+                Debug.DrawRay(transform.position + height, Vector3.down, Color.green);
+                Debug.DrawRay(transform.position + height + new Vector3(radius, 0, 0), Vector3.down, Color.green);
+                Debug.DrawRay(transform.position + height + new Vector3(-radius, 0, 0), Vector3.down, Color.green);
+                Debug.DrawRay(transform.position + height + new Vector3(0, 0, radius), Vector3.down, Color.green);
+                Debug.DrawRay(transform.position + height + new Vector3(0, 0, -radius), Vector3.down, Color.green);
+            }
+            else if(name == "Spider")
+            {
+                width = GetComponent<BoxCollider>().size;
+                Physics.Raycast(transform.position + height + new Vector3(width.x/2, 0, 0), Vector3.down, out hit[1], float.MaxValue, layerMask);
+                Physics.Raycast(transform.position + height + new Vector3(-width.x/2, 0, 0), Vector3.down, out hit[2], float.MaxValue, layerMask);
+                Physics.Raycast(transform.position + height + new Vector3(0, 0, width.z/2), Vector3.down, out hit[3], float.MaxValue, layerMask);
+                Physics.Raycast(transform.position + height + new Vector3(0, 0, -width.z/2), Vector3.down, out hit[4], float.MaxValue, layerMask);
+
+                Debug.DrawRay(transform.position + height, Vector3.down, Color.green);
+                Debug.DrawRay(transform.position + height + new Vector3(width.x / 2, 0, 0), Vector3.down, Color.green);
+                Debug.DrawRay(transform.position + height + new Vector3(-width.x / 2, 0, 0), Vector3.down, Color.green);
+                Debug.DrawRay(transform.position + height + new Vector3(0, 0, width.z / 2), Vector3.down, Color.green);
+                Debug.DrawRay(transform.position + height + new Vector3(0, 0, -width.z / 2), Vector3.down, Color.green);
+            }   
+
+            float distance = (hit[0].distance + hit[1].distance + hit[2].distance + hit[3].distance + hit[4].distance) / 5.0f;
+
+            //지면이 고르지 못할 때 떨림을 막기위해 간격을 넓게 함 / 중력 세기 감소
+            if (distance > 0.6f)
+            {
+                transform.Translate(new Vector3(0, -4.9f, 0) * Time.deltaTime);
+            }
+            else if (distance < 0.5f)
+            {
+                transform.Translate(new Vector3(0, 4.9f, 0) * Time.deltaTime);
+            }
+        }
+        else
+        {
+            //지면이 없으면 낙하
+            transform.Translate(new Vector3(0, -4.9f, 0) * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/Controllers/PlayerGravity.cs
+++ Assets/Scripts/Controllers/PlayerGravity.cs
@@ -6,29 +6,53 @@ public class PlayerGravity : MonoBehaviour
 {
     private int layerMask;
 
+    private Vector3 height;
+    private float radius;
+
+    private RaycastHit[] hit;
+
     // Start is called before the first frame update
     void Start()
     {
         layerMask = 1 << LayerMask.NameToLayer("Ground");
+
+        height = new Vector3(0, 0.55f, 0);
+        radius = GetComponent<CapsuleCollider>().radius;
+
+        hit = new RaycastHit[5];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Physics.SphereCast(transform.position + new Vector3(0, 0.9f, 0), 0.3f, Vector3.down, out RaycastHit hit, float.MaxValue, layerMask))
+        if (Physics.Raycast(transform.position + height, Vector3.down, out hit[0], float.MaxValue, layerMask))
         {
+            Physics.Raycast(transform.position + height + new Vector3(radius, 0, 0), Vector3.down, out hit[1], float.MaxValue, layerMask);
+            Physics.Raycast(transform.position + height + new Vector3(-radius, 0, 0), Vector3.down, out hit[2], float.MaxValue, layerMask);
+            Physics.Raycast(transform.position + height + new Vector3(0, 0, radius), Vector3.down, out hit[3], float.MaxValue, layerMask);
+            Physics.Raycast(transform.position + height + new Vector3(0, 0, -radius), Vector3.down, out hit[4], float.MaxValue, layerMask);
+
+            Debug.DrawRay(transform.position + height, Vector3.down, Color.green);
+            Debug.DrawRay(transform.position + height + new Vector3(radius, 0, 0), Vector3.down, Color.green);
+            Debug.DrawRay(transform.position + height + new Vector3(-radius, 0, 0), Vector3.down, Color.green);
+            Debug.DrawRay(transform.position + height + new Vector3(0, 0, radius), Vector3.down, Color.green);
+            Debug.DrawRay(transform.position + height + new Vector3(0, 0, -radius), Vector3.down, Color.green);
+
+            float distance = (hit[0].distance + hit[1].distance + hit[2].distance + hit[3].distance + hit[4].distance) / 5.0f;
+
             //지면이 고르지 못할 때 떨림을 막기위해 간격을 넓게 함
-            if (hit.distance > 0.75f)
+            if (distance > 0.6f)
             {
                 transform.Translate(new Vector3(0, -4.9f, 0) * Time.deltaTime);
             }
-            else if (hit.distance < 0.45f)
+            else if (distance < 0.5f)
             {
                 transform.Translate(new Vector3(0, 4.9f, 0) * Time.deltaTime);
             }
         }
         else
         {
+            //지면이 없으면 낙하
             transform.Translate(new Vector3(0, -4.9f, 0) * Time.deltaTime);
         }
     }

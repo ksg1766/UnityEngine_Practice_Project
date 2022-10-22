4d01419f20265dbe06645ba65e16f293b443d13c
ksg1766 <star1766@daum.net>
2022-10-23 01:57:10 +0900

    상점 UI 선택된 아이템 보여주는 기능
    선택된 아이템의 이름과 갯수를 저장하는 Dictionary형의 SelectedList를 이용. 다만 x를 눌러 상점 UI 종료 시 SelectedList가 정상적으로 Clear되지만 Interactive Zone을 벗어나 상점 UI가 종료 될 경우에는 Clear되지 않아 다시 상점을 열어 다른 아이템을 선택했을 때 이전의 SelectedList에 있던 아이템이 모두 출력되는 현상 발생. 아직 해결 못함. 어떻게 해야 할까...
    이해가 안가면 실행해서 상점을 이용해 보도록 하자.

--- Assets/Scripts/WeaponShop.cs
+++ Assets/Scripts/WeaponShop.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using static UnityEngine.UI.Image;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class WeaponShop : MonoBehaviour
 {
@@ -14,6 +18,12 @@ public class WeaponShop : MonoBehaviour
     };
 
     Dictionary<string, int> SelectedList = new Dictionary<string, int>();
+    //List<Sprite> ShowSelected = new List<Sprite>();
+    //List<string> ShowSelected_debug = new List<string>();
+
+    int selected_list_count = 1;
+
+    //Dictionary<Sprite, int> ShowSelected = new Dictionary<Sprite, int>();
 
     //public Action<PointerEventData> OnClickHandler = null;
 
@@ -23,14 +33,25 @@ public class WeaponShop : MonoBehaviour
         {
             Managers.Resource.Instantiate("Weapon_Shop", GameObject.Find("Item Shop Group").transform);
         }
+        SelectedList.Clear();
+        selected_list_count = 1;
     }
 
     public void Exit()
     {
+        //WeaponList.RemoveAll();
+        SelectedList.Clear();
+        //SelectedList = null;
+        selected_list_count = 1;
+        Debug.Log("Exit");
+        foreach (KeyValuePair<string, int> item in SelectedList)
+            Debug.Log(item.Key + " : " + item.Value);
+
+       
         if (GameObject.Find("Weapon_Shop") != null)
             Managers.Resource.Destroy(GameObject.Find("Weapon_Shop"));
-        WeaponList.Clear();
-        SelectedList.Clear();
+
+        
     }
 
     // 작업 중... : 아이템 선택시 선택된 아이템 창(버튼)에 아이템 등록하는 내용 추가 필요
@@ -39,14 +60,14 @@ public class WeaponShop : MonoBehaviour
         if (EventSystem.current.currentSelectedGameObject.name == "Button_SelectPickAxe")
         {
             Debug.Log("PickAxe selected");
-            
+
             if (SelectedList.ContainsKey("PickAxe"))
                 SelectedList["PickAxe"] += 1;
             else
                 SelectedList.Add("PickAxe", 1);
 
             // 리스트 잘 올라갔는지 확인용
-            foreach(KeyValuePair<string, int> item in SelectedList)
+            foreach (KeyValuePair<string, int> item in SelectedList)
                 Debug.Log(item.Key + " : " + item.Value);
 
             // if(SelectedList.Count < 3)
@@ -57,7 +78,7 @@ public class WeaponShop : MonoBehaviour
         if (EventSystem.current.currentSelectedGameObject.name == "Button_SelectSteelAxe")
         {
             Debug.Log("SteelAxe selected");
-            
+
             if (SelectedList.ContainsKey("SteelAxe"))
                 SelectedList["SteelAxe"] += 1;
             else
@@ -80,6 +101,23 @@ public class WeaponShop : MonoBehaviour
             // 리스트 잘 올라갔는지 확인용
             foreach (KeyValuePair<string, int> item in SelectedList)
                 Debug.Log(item.Key + " : " + item.Value);
+        }
+
+        selected_list_count = 1;
+
+        // 예약된 아이템 이미지 딕셔너리에 출력
+        foreach (KeyValuePair<string, int> item in SelectedList)
+        {
+            //ShowSelected.Add(Resources.Load<Sprite>($"Icons/{item.Key}"), item.Value);
+            if (Resources.Load<Sprite>($"Icons/{item.Key}") == null)
+                Debug.Log($"{item.Key} = null");
+
+            GameObject temp = GameObject.Find($"Selected Item {selected_list_count}").transform.Find($"weaponshop_selected{selected_list_count}").gameObject;
+            if(!temp.activeSelf)
+                temp.SetActive(true);
+
+            temp.GetComponent<Image>().sprite = Resources.Load<Sprite>($"Icons/{item.Key}") as Sprite;
+            selected_list_count++;
         }
     }
 

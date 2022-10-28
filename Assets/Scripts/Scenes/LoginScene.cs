8753e9d2f97d74ec8987a3de1193986c8b0cc900
SunKyu Park <chopr159@gmail.com>
2022-10-29 01:40:40 +0900

    LoginSystem Clear

--- Assets/Scripts/Scenes/LoginScene.cs
+++ Assets/Scripts/Scenes/LoginScene.cs
@@ -1,27 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UGS;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 public class LoginScene : BaseScene
 {
+    [Header("TempLOginScreen")]
+    public TMP_InputField InputField_ID;
+    public TMP_InputField InputField_Password;
+    public Button PlayButton;
+    public bool IsLogin = false;
     protected override void Init()
     {
         base.Init();
-
         SceneType = Define.Scene.Login;
     }
-
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
+        if(IsLogin)
         {
             Managers.Scene.LoadScene(Define.Scene.TestScene);
         }
     }
-
     public override void Clear()
     {
-        Debug.Log("LoginScene Clear!");
+        UnityGoogleSheet.Load<Userinfo.User>();
+        foreach(var value in Userinfo.User.UserList)
+        {
+            if(value.ID == InputField_ID.text && value.Password == InputField_Password.text)
+            {
+                Debug.Log(InputField_ID.text);
+                Debug.Log(InputField_Password.text);
+                Debug.Log("LoginScene Clear!");
+                IsLogin = true;
+                break;
+            }
+        }
     }
 }
